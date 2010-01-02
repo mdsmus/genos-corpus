b8dedbfe129bd1f0b77cabf5c6cd0490e59d20f1
Pedro Kroger <pedro.kroger@gmail.com>
2010-01-01 22:42:42 -0800

    see both versions at the same time

--- music/bach-chorales/make.sh
+++ music/bach-chorales/make.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-lilypond --ps $1.ly && gv $1.ps
+lilypond ly/$1.ly
+mv $1.pdf $1-lily.pdf
+hum2abc kern/$1.krn > $1.abc
+abcm2ps -O $1.ps $1.abc
+ps2pdf $1.ps
+mv $1.pdf $1-krn.pdf
+xpdf -geometry 800x345+0+0 $1-lily.pdf &
+xpdf -geometry 800x345+0+400 $1-krn.pdf &
